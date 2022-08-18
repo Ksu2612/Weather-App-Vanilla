b2bcdbb30387c71cc85b4bb197c59ae86a750166
Ksu2612 <110895528+Ksu2612@users.noreply.github.com>
2022-08-18 14:44:51 +0300

    started making APi

---
+++ app.js
@@ -0,0 +1,3 @@
+let apiKey = "0e1aeea50510eae32c5d41486185c026";
+let apiUrl = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={apiKey}`;
+console.log(apiUrl);
