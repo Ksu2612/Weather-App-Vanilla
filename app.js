a0ba0819fdbf2b5a1388fb5a300c91e84deed7c4
Ksu2612 <110895528+Ksu2612@users.noreply.github.com>
2022-08-18 15:32:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 let apiKey = "0e1aeea50510eae32c5d41486185c026";
-let apiUrl = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}`;
 console.log(apiUrl);
