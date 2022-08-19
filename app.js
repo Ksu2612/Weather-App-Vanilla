341884cf1a312390c312f875e75be2be8c1871ca
Ksu2612 <110895528+Ksu2612@users.noreply.github.com>
2022-08-19 19:57:34 +0300

    Added the search engine

--- app.js
+++ app.js
@@ -41,8 +41,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "0e1aeea50510eae32c5d41486185c026";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "0e1aeea50510eae32c5d41486185c026";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Milan");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
